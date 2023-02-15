6fbf3e9dcb3f2cb13483906a3eef19aef467f5e0
rkamysz <radoslaw.kamysz@gmail.com>
2023-02-15 10:11:01 +0100

    add contracts components

--- src/processor/processor.bindings.ts
+++ src/processor/processor.bindings.ts
@@ -8,6 +8,18 @@ import MFederationActionProcessor from './processors/m-federation/m-federation.a
 import MFederationDeltaProcessor from './processors/m-federation/m-federation.delta-processor';
 import FederationActionProcessor from './processors/federation/federation.action-processor';
 import FederationDeltaProcessor from './processors/federation/federation.delta-processor';
+import SFederationActionProcessor from './processors/s-federation/s-federation.action-processor';
+import SFederationDeltaProcessor from './processors/s-federation/s-federation.delta-processor';
+import AwlndratingsActionProcessor from './processors/awlndratings/awlndratings.action-processor';
+import AwlndratingsDeltaProcessor from './processors/awlndratings/awlndratings.delta-processor';
+import BoostWorldsActionProcessor from './processors/boost-worlds/boost-worlds.action-processor';
+import BoostWorldsDeltaProcessor from './processors/boost-worlds/boost-worlds.delta-processor';
+import NftmtWorldsActionProcessor from './processors/nftmt-worlds/nftmt-worlds.action-processor';
+import NftmtWorldsDeltaProcessor from './processors/nftmt-worlds/nftmt-worlds.delta-processor';
+import OtherWorldsActionProcessor from './processors/other-worlds/other-worlds.action-processor';
+import OtherWorldsDeltaProcessor from './processors/other-worlds/other-worlds.delta-processor';
+import PtpxyWorldsActionProcessor from './processors/ptpxy-worlds/ptpxy-worlds.action-processor';
+import PtpxyWorldsDeltaProcessor from './processors/ptpxy-worlds/ptpxy-worlds.delta-processor';
 
 const container = new WorkerContainer();
 
@@ -23,5 +35,23 @@ container.bind(ProcessorLabel.MFederationDeltaProcessor, MFederationDeltaProcess
 // 'federation'
 container.bind(ProcessorLabel.FederationActionProcessor, FederationActionProcessor);
 container.bind(ProcessorLabel.FederationDeltaProcessor, FederationDeltaProcessor);
+// 's.federation'
+container.bind(ProcessorLabel.SFederationActionProcessor, SFederationActionProcessor);
+container.bind(ProcessorLabel.SFederationDeltaProcessor, SFederationDeltaProcessor);
+// 'other.worlds'
+container.bind(ProcessorLabel.OtherWorldsActionProcessor, OtherWorldsActionProcessor);
+container.bind(ProcessorLabel.OtherWorldsDeltaProcessor, OtherWorldsDeltaProcessor);
+// 'boost.worlds'
+container.bind(ProcessorLabel.BoostWorldsActionProcessor, BoostWorldsActionProcessor);
+container.bind(ProcessorLabel.BoostWorldsDeltaProcessor, BoostWorldsDeltaProcessor);
+// 'ptpxy.worlds'
+container.bind(ProcessorLabel.PtpxyWorldsActionProcessor, PtpxyWorldsActionProcessor);
+container.bind(ProcessorLabel.PtpxyWorldsDeltaProcessor, PtpxyWorldsDeltaProcessor);
+// 'nftmt.worlds'
+container.bind(ProcessorLabel.NftmtWorldsActionProcessor, NftmtWorldsActionProcessor);
+container.bind(ProcessorLabel.NftmtWorldsDeltaProcessor, NftmtWorldsDeltaProcessor);
+// 'awlndratings'
+container.bind(ProcessorLabel.AwlndratingsActionProcessor, AwlndratingsActionProcessor);
+container.bind(ProcessorLabel.AwlndratingsDeltaProcessor, AwlndratingsDeltaProcessor);
 
 export default container;
---
+++ src/processor/processors/nftmt-worlds/nftmt-worlds.action-processor.ts
@@ -0,0 +1,23 @@
+import { log } from '@alien-worlds/api-core';
+import {
+  ActionTraceProcessor,
+  ProcessorSharedData,
+  ProcessorTaskModel,
+} from '@alien-worlds/api-history-tools';
+
+type ContractData = { [key: string]: unknown };
+
+export default class NftmtWorldsActionProcessor extends ActionTraceProcessor<ContractData> {
+  public async run(
+    model: ProcessorTaskModel,
+    sharedData: ProcessorSharedData
+  ): Promise<void> {
+    try {
+      await super.run(model, sharedData);
+      
+  } catch (error){
+    log(error);
+    this.reject(error);
+  }
+}
+}
